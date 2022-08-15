a1aea9d4b662f84eb5ded767678361d586df9663
GonzaloSaad <saad.gonzalo.ale@gmail.com>
2022-08-15 17:21:51 -0300

    chore: add controller

---
+++ src/main/java/org/github/guppy/backend/controller/TestController.java
@@ -0,0 +1,15 @@
+package org.github.guppy.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
+
