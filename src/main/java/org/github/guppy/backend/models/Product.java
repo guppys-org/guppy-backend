6c0521a148fbbbb27aa6c78e190fe5d7f86c8154
pedro9747@gmail.com <pedro9747@gmail.com>
2022-09-09 21:25:43 -0300

    agrego modelo DB

---
+++ src/main/java/org/github/guppy/backend/models/Product.java
@@ -0,0 +1,39 @@
+package org.github.guppy.backend.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column
+    private String name;
+
+    @Column
+    private double price;
+
+    @Column
+    private UUID businessId;
+
+    @Column
+    private String imageUrl;
+
+    @Column
+    private String createdAt;
+
+    @Column
+    private String updatedAt;
+
+}
