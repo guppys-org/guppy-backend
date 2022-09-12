8796654cf0a787d8b4df10d7a3f201a3fdeb1fce
GonzaloSaad <saad.gonzalo.ale@gmail.com>
2022-09-12 18:42:32 -0300

    ci: add tests and change to postgres

--- src/main/java/org/github/guppy/backend/controller/ExampleController.java
+++ src/main/java/org/github/guppy/backend/controller/ExampleController.java
@@ -1,14 +1,15 @@
 package org.github.guppy.backend.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class ExampleController {
 
     @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("Greetings from Spring Boot!");
     }
 
 }
