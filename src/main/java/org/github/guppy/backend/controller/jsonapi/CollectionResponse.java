2506d384d140623dcf5d8a15289fcb9c4acf00fb
GonzaloSaad <saad.gonzalo.ale@gmail.com>
2022-09-13 18:31:50 -0300

    chore: Add CollectionResponse

---
+++ src/main/java/org/github/guppy/backend/controller/jsonapi/CollectionResponse.java
@@ -0,0 +1,22 @@
+package org.github.guppy.backend.controller.jsonapi;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+/***
+ * Class for implementing JSONAPI <a href="https://jsonapi.org/format/#document-structure">spec</a> Collection Responses
+ * @param <T> the body type
+ */
+public class CollectionResponse<T> {
+    @JsonProperty("data")
+    private final List<T> data;
+
+    public CollectionResponse(List<T> data) {
+        this.data = data;
+    }
+
+    public static <R> CollectionResponse<R> of(List<R> data){
+        return new CollectionResponse<>(data);
+    }
+}
