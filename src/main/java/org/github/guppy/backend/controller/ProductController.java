8c55973ea2b7623840723469bebd6c343e22958b
pedro9747@gmail.com <pedro9747@gmail.com>
2022-09-13 16:33:38 -0300

    Agrego ProductController

---
+++ src/main/java/org/github/guppy/backend/controller/ProductController.java
@@ -0,0 +1,14 @@
+package org.github.guppy.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("v1/products")
+    public ResponseEntity<String> products() {
+        return ResponseEntity.ok("Here you have your products. What where you expecting, unicorns?");
+    }
+}
