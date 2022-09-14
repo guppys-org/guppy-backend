02f41704189303cd726af3364f26f5d37d4467e7
pedro9747@gmail.com <pedro9747@gmail.com>
2022-09-14 00:05:15 -0300

    Agrego unit test para ProductController

---
+++ src/test/java/org/github/guppy/backend/controller/ProductControllerTest.java
@@ -0,0 +1,33 @@
+package org.github.guppy.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class ProductControllerTest {
+
+    @InjectMocks
+    private ProductController controller;
+
+    @Test
+    public void textExampleController(){
+        //Given
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+        //When
+        ResponseEntity<String> responseEntity = controller.products();
+
+        //Then
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        assertThat(responseEntity.getBody()).isEqualTo("Here you have your products. What where you expecting, unicorns?");
+    }
+}
