9b55004192d28a19965a2ec5d6ad5364b7f45e2c
Gonzalo Saad <saad.gonzalo.ale@gmail.com>
2022-09-14 00:15:24 -0300

    chore: Add CollectionResponse (#11)
    * chore: Add CollectionResponse
    
    * chore: add end of line space
    
    * chore: rename test

---
+++ src/test/java/org/github/guppy/backend/controller/jsonapi/CollectionResponseTest.java
@@ -0,0 +1,32 @@
+package org.github.guppy.backend.controller.jsonapi;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class CollectionResponseTest {
+
+    @Test
+    public void testCollectionResponse() throws JsonProcessingException {
+        // Given
+        List<String> body = new ArrayList<>();
+        body.add("Element 1");
+        body.add("Element 2");
+
+        // When
+        CollectionResponse<String> collectionResponse = CollectionResponse.of(body);
+
+        // Then
+        ObjectMapper mapper = new ObjectMapper();
+
+        String json = mapper.writeValueAsString(collectionResponse);
+        assertThat(json).isEqualTo("{\"data\":[\"Element 1\",\"Element 2\"]}");
+    }
+
+}
