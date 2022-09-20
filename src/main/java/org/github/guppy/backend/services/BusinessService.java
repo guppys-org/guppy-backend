d3885d9caa7ce54a8374cb7813fbddf1abcdd570
facuromero1 <facuromero4@gmail.com>
2022-09-20 16:02:53 -0300

    add BusinessService

---
+++ src/main/java/org/github/guppy/backend/services/BusinessService.java
@@ -0,0 +1,25 @@
+package org.github.guppy.backend.services;
+
+import org.github.guppy.backend.models.Business;
+import org.github.guppy.backend.repositories.BusinessRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BusinessService {
+    @Autowired
+    private BusinessRepository businessRepository;
+
+
+
+    public List<Business> getBusiness(){
+        List<Business> businesses = new ArrayList<Business>();
+        Iterable<Business> result = businessRepository.findAll();
+        result.forEach(businesses::add);
+        return businesses;
+
+    }
+}
